1d9dddb52622eabf0280006f87fd279c80229dd1
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-20 02:24:51 +0530

    Update Array_min.java

--- Array_min.java
+++ Array_min.java
@@ -27,27 +27,3 @@ public class Array_input {
 
     }
 }
-import java.util.Scanner;
-
-public class Array_input {
-    public static void main(String[] args)
-    {
-        int array[] = new int[5], min;
-        System.out.println("Enter the element of an array: ");
-        Scanner p = new Scanner(System.in);
-        for (int i=0; i <array.length; i++)
-        {
-            array[i]=p.nextInt();
-        }
-        min=array[0];
-        for (int j=0; j < array.length; j++)
-        {
-            if (array[j]<min)
-            {
-                min=array[j];
-            }
-        }
-        System.out.println(min);
-
-    }
-}
