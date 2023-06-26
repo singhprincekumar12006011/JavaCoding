d20eec88f6a8b17f421cb5c87f2b75a65829b037
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-27 04:35:07 +0530

    Add files via upload

---
+++ String_Char_Array_1.java
@@ -0,0 +1,9 @@
+public class String_Char_Array_1 {
+    public static void main(String[] args)
+    {
+        char[] CharArray = {'P','r','i','n','c','e'};
+        String str= new String(CharArray);
+
+        System.out.println("The string is : " + str);
+    }
+}
