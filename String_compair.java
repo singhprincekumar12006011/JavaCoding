762ffe9513583cfc15444da7eba5839d47f8fba7
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-29 06:22:48 +0530

    Add files via upload

---
+++ String_compair.java
@@ -0,0 +1,11 @@
+public class String_compair {
+    public static void main(String[] agrs)
+    {
+        String str1 = "Hello World";
+        String str2 = "hello world";
+        //this is the other way to compair the strings
+        // if it matches then is it return true other wise it return false.
+        System.out.println(str1 == str2);
+    }
+    
+}
