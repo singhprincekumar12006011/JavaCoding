81f6095952ae4f58018cfb6cef30069916788495
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-29 06:11:25 +0530

    initial commit

---
+++ String_Function_equals.java
@@ -0,0 +1,11 @@
+public class String_Function_equals {
+    public static void main(String[] args)
+    {
+        String str1 = "Prince Kumar";
+        String str2 = "Prince";
+        System.out.println(str1.equals(str2));
+
+        // the equal to function return false if it not matches and it return true if it is matches.
+    }
+    
+}
