ee5b631e58fd8e3aad01e225a5261f45a10dd0ba
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-06-29 05:55:59 +0530

    initial commit

---
+++ String_Function_Contains.java
@@ -0,0 +1,11 @@
+public class String_Function_Contains {
+    
+    public static void main(String[] args)
+    {
+        String str1 = "Prince Kumar";
+        String str2 = "Prince";
+
+        System.out.println(str1.contains(str2));
+        //return true
+    }
+}
