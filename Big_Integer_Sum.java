be3a4b2c8f31fe2025e456678d78bca3767e8453
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-07-02 12:47:56 +0530

    initial commit

---
+++ Big_Integer_Sum.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+public class Big_Integer_Sum {
+    public static void main(String[] args)
+    {
+        BigInteger x = new BigInteger("10");
+        BigInteger y = new BigInteger("20");
+
+        BigInteger c = x.add(y);
+        System.out.println(c);
+    }
+
+    
+}
