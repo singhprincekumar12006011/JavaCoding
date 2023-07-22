7540ec99af29e8b34cbb1eb20313ac551eb496df
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-07-22 20:50:59 +0530

    initial commit

---
+++ ArrayLsit_4.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayLsit_4 {
+    public static void main(String[] args)
+    {
+        ArrayList <String> str = new ArrayList<String>();
+        str.add("Prince Singh");
+        str.add("Sanjeet Singh");
+        str.add("Manish Singh");
+        str.add("Adarsh Sah");
+        System.out.println(str);
+    
+    }
+}
