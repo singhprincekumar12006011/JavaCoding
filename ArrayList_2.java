4dc33c85d62258fdcdca6922bd2634bfbfa5a95b
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-07-22 20:50:18 +0530

    initial commit

---
+++ ArrayList_2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayList_2 {
+    public static void main(String[] args)
+    {
+        ArrayList <Integer> arrint = new ArrayList<Integer>();
+        arrint.add(12);
+        arrint.add(15);
+        arrint.add(20);
+        arrint.add(25);
+        arrint.add(30);
+        System.out.println(arrint);
+    }
+}
