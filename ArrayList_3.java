d44f26ba89b5f40b3b4b6647425dfc4ce95a64bc
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-07-22 20:50:37 +0530

    initial commit

---
+++ ArrayList_3.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ArrayList_3 {
+    public static void main(String[] args)
+    {
+        ArrayList <Integer> arr = new ArrayList<Integer>();
+        arr.add(10);
+        arr.add(15);
+        arr.add(20);
+        arr.add(25);
+        arr.add(30);
+        arr.add(35);
+        System.out.println(arr);
+        System.out.println("This is an example of an ArrayList :");
+    }
+    
+}
